d767620d05dfd1b7b44b089222f5007884818b6c
Richard-m-j <richardj815165@gmail.com>
2025-08-10 15:02:22 +0530

    Refactor main.tf by removing unnecessary 'depends_on' statements for Kubernetes and Helm providers to streamline EKS cluster configuration.

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,9 +27,6 @@ provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.this.token
-
-  # Prevent "no such host" errors
-  depends_on = [module.eks]
 }
 
 provider "helm" {
@@ -38,14 +35,12 @@ provider "helm" {
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.this.token
   }
-
-  depends_on = [module.eks]
 }
 
-# Get authentication token for EKS
 data "aws_eks_cluster_auth" "this" {
   name = module.eks.cluster_name
 }
+
 
 # EKS Cluster
 module "eks" {
