e92a1748ff15b830a05d4ff0401808bc1bd884b6
Richard-m-j <richardj815165@gmail.com>
2025-08-10 16:43:08 +0530

    Refactor Helm release configuration in load-balancer.tf to use a list of objects for 'set' arguments, improving syntax correctness and readability.

--- terraform/load-balancer.tf
+++ terraform/load-balancer.tf
@@ -25,16 +25,19 @@ resource "helm_release" "aws_load_balancer_controller" {
   namespace  = "kube-system"
   version    = "1.7.2" # Specify a chart version compatible with K8s 1.32
 
-  set {
-    name  = "clusterName"
-    value = var.cluster_name
-  }
-  set {
-    name  = "serviceAccount.create"
-    value = "false" # We use the IAM role from the module above
-  }
-  set {
-    name  = "serviceAccount.name"
-    value = "aws-load-balancer-controller"
-  }
+  # Corrected syntax: 'set' is an argument that takes a list of objects.
+  set = [
+    {
+      name  = "clusterName"
+      value = var.cluster_name
+    },
+    {
+      name  = "serviceAccount.create"
+      value = "false" # We use the IAM role from the module above
+    },
+    {
+      name  = "serviceAccount.name"
+      value = "aws-load-balancer-controller"
+    }
+  ]
 }
