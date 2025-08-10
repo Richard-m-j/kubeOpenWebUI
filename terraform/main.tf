28dedc5666821d10c1e3e079d5bb6fc2c4fc3e24
Richard-m-j <richardj815165@gmail.com>
2025-08-10 12:42:39 +0530

    Remove commented-out cluster_version line from EKS module configuration in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,8 +48,8 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.8.4"
 
-  cluster_name    = var.cluster_name
-  cluster_version = "1.29"
+  cluster_name = var.cluster_name
+  # cluster_version = "1.29" # <-- Ensure this line is removed
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
