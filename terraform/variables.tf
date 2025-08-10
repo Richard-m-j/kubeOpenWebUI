2d0093b508f6ba4c6aa36c8ee28bb1e9f2fe24a7
Richard-m-j <richardj815165@gmail.com>
2025-08-10 11:06:31 +0530

    region change

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,7 +3,7 @@
 variable "aws_region" {
   description = "The AWS region to deploy the resources in."
   type        = string
-  default     = "us-east-1"
+  default     = "us-west-1"
 }
 
 variable "cluster_name" {
